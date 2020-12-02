ecf486e5f0a922d18a7ae127ae475941b3825737
ivan <ibanb040@gmail.com>
2020-12-02 16:16:39 -0500

    interfaz crear libro terminada

---
+++ app/src/main/java/com/example/tallermaterial/CrearLibro.java
@@ -0,0 +1,14 @@
+package com.example.tallermaterial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearLibro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_libro);
+    }
+}
