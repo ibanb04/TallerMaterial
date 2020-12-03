ef530f1ecc455e552f4a1ae4c02d82c0ed439efd
ivan <ibanb040@gmail.com>
2020-12-03 00:13:23 -0500

    crear libros terminado con firebase sin validaciones

--- app/src/main/java/com/example/tallermaterial/CrearLibro.java
+++ app/src/main/java/com/example/tallermaterial/CrearLibro.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -11,10 +12,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class CrearLibro extends AppCompatActivity {
     private EditText ISBN, nombre, autor;
     private ImageView foto;
+    private Uri uri;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,10 @@ public class CrearLibro extends AppCompatActivity {
         ISBN = findViewById(R.id.txtISBN);
         nombre = findViewById(R.id.txtNombre);
         autor = findViewById(R.id.txtAutor);
+        foto = findViewById(R.id.imgFotoSeleccionada);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+
     }
 
     public void guardar(View v){
@@ -33,14 +44,23 @@ public class CrearLibro extends AppCompatActivity {
         isbn = ISBN.getText().toString();
         nom = nombre.getText().toString();
         aut = autor.getText().toString();
+        id = Datos.getId();
         imp = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        l = new Libro(isbn, nom, aut, "");
+        l = new Libro(isbn, nom, aut,id);
         l.guardar();
         imp.hideSoftInputFromWindow(ISBN.getWindowToken(),0);
         limpiar();
+        subirFoto(id);
         Snackbar.make(v, R.string.msj_guardado_exitosamente,
                 Snackbar.LENGTH_LONG).show();
+
+    }
+
+    public void subirFoto(String id){
+        StorageReference child = storageReference.child(id);
+        UploadTask uploadTask = child.putFile(uri);
+
     }
 
     public void limpiar(View v){
@@ -52,11 +72,30 @@ public class CrearLibro extends AppCompatActivity {
         nombre.setText("");
         autor.setText("");
         ISBN.requestFocus();
+        foto.setImageResource(android.R.drawable.ic_menu_gallery);
     }
 
     public void onBackPressed(){
         finish();
         Intent i = new Intent(CrearLibro.this, MainActivity.class);
         startActivity(i);
+    }
+
+    public void seleccionarFoto(View v){
+        Intent in = new Intent();
+        in.setType("image/*");
+        in.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(in,
+                "seleccione la foto de la persona"), 1);
+    }
+
+    protected void onActivityResult(int requesCode, int resultCode, Intent data){
+        super.onActivityResult(requesCode, resultCode, data);
+        if(requesCode == 1){
+            uri = data.getData();
+            if (uri != null){
+                foto.setImageURI(uri);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tallermaterial/Datos.java
+++ app/src/main/java/com/example/tallermaterial/Datos.java
@@ -1,15 +1,26 @@
 package com.example.tallermaterial;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class Datos {
-    public static ArrayList<Libro> libros = new ArrayList();
+    private static String db = "Libros";
+    private static DatabaseReference databaseReference =
+            FirebaseDatabase.getInstance().getReference();
+
+    private static ArrayList<Libro> libros = new ArrayList();
+
+    public static String getId(){
+        return databaseReference.push().getKey();
+    }
 
     public static void guardar(Libro l){
-        libros.add(l);
+        databaseReference.child(db).child(l.getId()).setValue(l);
     }
 
-    public static ArrayList<Libro> obtener(){
-        return libros;
-    }
+    public static ArrayList<Libro> obtener(){ return libros; }
+
+
 }
